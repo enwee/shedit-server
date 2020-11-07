96695eb2f881d5e5319f9e98fcb687cff38cad86
enwee <58569809+enwee@users.noreply.github.com>
2020-11-07 14:06:15 +0800

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { mongoOptions } = require("./constants");
 
 const dbName = "shedit";
-const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/" + dbName;
+const dbUrl = process.env.DB_URI || "mongodb://localhost:27017/" + dbName;
 
 mongoose
   .connect(dbUrl, mongoOptions)
